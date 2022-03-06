fd7b74630703d196294813269db05ec5b4f48528
goztrk <gokhan@eg-ozturk.com>
2022-03-06 21:10:37 +0300

    Add nvchad custom plugin config

---
+++ nvim/plugins/configs.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "lua",
+    "vim",
+    "html",
+    "css",
+    "javascript",
+    "json",
+    "python",
+    "bash",
+  },
+}
+
+return M
+
