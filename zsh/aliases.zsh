04e21543883d055f49f63cf8fbcba9c9525ded9f
goztrk <gokhan@eg-ozturk.com>
2022-03-06 19:40:15 +0300

    Add zsh module

---
+++ zsh/aliases.zsh
@@ -0,0 +1,10 @@
+alias reload!='. ~/.zshrc'
+
+# Good 'ol Clear Screen command
+alias cls='clear'
+
+# Moving up from directory
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias cd..='cd ..'
