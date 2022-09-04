da76d0361b30b109fd25f7d930d0675306990a21
goztrk <gokhan@eg-ozturk.com>
2022-09-04 12:16:36 +0300

    Add mappings for nvim

--- nvim/mappings.lua
+++ nvim/mappings.lua
@@ -1,7 +1,24 @@
 local M = {}
 
 M.gokhan = {
+  i = {
+    -- Save
+    ["<C-s>"] = { "<cmd> w <CR>", "save file" },
+  },
   n = {
+    -- Turkish character mappings
+    ["ğ"] = {"["},
+    ["Ğ"] = {"{"},
+    ["ü"] = {"]"},
+    ["Ü"] = {"}"},
+    ["ı"] = {"i"},
+    ["ş"] = {"/"},
+
+    -- git
+    ["<leader>ga"] = {"<cmd> :!git add % <CR>", "GIT: stage current file"},
+  },
+  v = {
+    -- Turkish character mappings
     ["ğ"] = {"["},
     ["Ğ"] = {"{"},
     ["ü"] = {"]"},
