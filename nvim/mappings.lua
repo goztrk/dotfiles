9ef9629818041c7606a2a85d2f8ae80b57b9e19d
goztrk <gokhan@eg-ozturk.com>
2022-09-15 00:03:08 +0300

    Make modifications on neovim configuration

--- nvim/mappings.lua
+++ nvim/mappings.lua
@@ -25,6 +25,30 @@ M.gokhan = {
     ["Ü"] = {"}"},
     ["ı"] = {"i"},
     ["ş"] = {"/"},
+
+    -- Don't leave the visual mode while indenting or outdenting.
+    ["<"] = {"<gv"},
+    [">"] = {">gv"},
+  },
+}
+
+M.comment = {
+  plugin = true,
+
+  n = {
+    ["<leader>ö"] = {
+      function()
+        require("Comment.api").toggle.linewise.current()
+      end,
+      "toggle comment",
+    },
+  },
+
+  v = {
+    ["<leader>ö"] = {
+      "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+      "toggle comment",
+    },
   },
 }
 
