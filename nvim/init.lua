db8725860e1010dd97488ba4358958c3f75314b1
goztrk <gokhan@eg-ozturk.com>
2022-09-20 01:13:18 +0300

    Add basic keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,2 @@
 require "user.hot-reload"
+require "user.keymaps"
---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,39 @@
+M = {}
+local opts = { noremap = true, silent = true }
+
+local termOpts = { silent = true }
+
+-- Shorten function name
+keymap = vim.api.nvim_set_keymap
+
+-- Remap space as leader key
+keymap("n", "<Space>", "", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+keymap("n", "<C-Space>", "<cmd>WhichKey \\<leader><cr>", opts)
+keymap("n", "<C-i>", "<C-i>", opts)
+
+-- INSERT MODE --
+-- Go to beggining and end
+keymap("i", "<C-b>", "<ESC>^i", opts)
+keymap("i", "<C-e>", "<End>", opts)
+
+-- Navigate within insert mode
+keymap("i", "<C-h>", "<Left>", opts)
+keymap("i", "<C-l>", "<Right>", opts)
+keymap("i", "<C-j>", "<Down>", opts)
+keymap("i", "<C-k>", "<Up>", opts)
+
+
+-- NORMAL MODE --
+keymap("n", "<C-s>", ":w<CR>", opts)
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<Tab>", ":bnext<CR>", opts)
+keymap("n", "<S-Tab>", ":bprevious<CR>", opts)
+-- keymap("", "", "", opts)
