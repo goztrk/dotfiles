298ebd92b67baa028a72bca92250b8de338b788b
goztrk <gokhan@eg-ozturk.com>
2022-09-20 14:25:51 +0300

    Add keymaps to whichkey

--- nvim/lua/user/whichkey.lua
+++ nvim/lua/user/whichkey.lua
@@ -1,4 +1,4 @@
-local status_ok, which_key = pcall(require, "which-key")
+local status_ok, which_key = pcall(require, 'which-key')
 if not status_ok then
   return
 end
@@ -30,23 +30,23 @@ local setup = {
   key_labels = {
     -- override the label used to display some keys. It doesn't effect WK in any other way.
     -- For example:
-    -- ["<space>"] = "SPC",
-    ["<leader>"] = "SPC",
-    -- ["<cr>"] = "RET",
-    -- ["<tab>"] = "TAB",
+    -- ['<space>'] = 'SPC',
+    ['<leader>'] = 'SPC',
+    -- ['<cr>'] = 'RET',
+    -- ['<tab>'] = 'TAB',
   },
   icons = {
-    breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-    separator = "➜", -- symbol used between a key and it's label
-    group = "+", -- symbol prepended to a group
+    breadcrumb = '»', -- symbol used in the command line area that shows your active key combo
+    separator = '➜', -- symbol used between a key and it's label
+    group = '+', -- symbol prepended to a group
   },
   popup_mappings = {
-    scroll_down = "<c-d>", -- binding to scroll down inside the popup
-    scroll_up = "<c-u>", -- binding to scroll up inside the popup
+    scroll_down = '<c-d>', -- binding to scroll down inside the popup
+    scroll_up = '<c-u>', -- binding to scroll up inside the popup
   },
   window = {
-    border = "rounded", -- none, single, double, shadow
-    position = "bottom", -- bottom, top
+    border = 'rounded', -- none, single, double, shadow
+    position = 'bottom', -- bottom, top
     margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
     padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
     winblend = 0,
@@ -55,26 +55,26 @@ local setup = {
     height = { min = 4, max = 25 }, -- min and max height of the columns
     width = { min = 20, max = 50 }, -- min and max width of the columns
     spacing = 3, -- spacing between columns
-    align = "center", -- align columns left, center or right
+    align = 'center', -- align columns left, center or right
   },
   ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
-  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+  hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', 'call', 'lua', '^:', '^ ' }, -- hide mapping boilerplate
   show_help = false, -- show help message on the command line when the popup is visible
-  -- triggers = "auto", -- automatically setup triggers
-  -- triggers = {"<leader>"} -- or specify a list manually
+  -- triggers = 'auto', -- automatically setup triggers
+  -- triggers = {'<leader>'} -- or specify a list manually
 
   triggers_blacklist = {
     -- list of mode / prefixes that should never be hooked by WhichKey
     -- this is mostly relevant for key maps that start with a native binding
     -- most people should not need to change this
-    i = { "j", "k" },
-    v = { "j", "k" },
+    i = { 'j', 'k' },
+    v = { 'j', 'k' },
   },
 }
 
 local opts = {
-  mode = "n", -- NORMAL mode
-  prefix = "<leader>",
+  mode = 'n', -- NORMAL mode
+  prefix = '<leader>',
   buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
   silent = true, -- use `silent` when creating keymaps
   noremap = true, -- use `noremap` when creating keymaps
@@ -82,8 +82,8 @@ local opts = {
 }
 
 local m_opts = {
-  mode = "n", -- NORMAL mode
-  prefix = "m",
+  mode = 'n', -- NORMAL mode
+  prefix = 'm',
   buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
   silent = true, -- use `silent` when creating keymaps
   noremap = true, -- use `noremap` when creating keymaps
@@ -97,10 +97,99 @@ local mappings = {
   q = { '<cmd>lua require("user.functions").smart_quit()<CR>', "Quit" },
   v = { "<cmd>vsplit<cr>", "vsplit" },
   h = { "<cmd>split<cr>", "split" },
+  ["/"] = { '<cmd> lua require("Comment.api").toggle.linewise.current()<CR>', 'Comment Line'},
 
   n = {
     n = { '<cmd>set nu!<CR>', 'Toggle line number' },
     r = { '<cmd>set rnu!<CR>', 'Toggle relative number' },
+  },
+
+  f = {
+    name = 'Find',
+    b = { '<cmd>Telescope git_branches<cr>', 'Checkout branch' },
+    c = { '<cmd>Telescope colorscheme<cr>', 'Colorscheme' },
+    f = { '<cmd>Telescope find_files<cr>', 'Find files' },
+    t = { '<cmd>Telescope live_grep<cr>', 'Find Text' },
+    s = { '<cmd>Telescope grep_string<cr>', 'Find String' },
+    h = { '<cmd>Telescope help_tags<cr>', 'Help' },
+    H = { '<cmd>Telescope highlights<cr>', 'Highlights' },
+    i = { "<cmd>lua require('telescope').extensions.media_files.media_files()<cr>", 'Media' },
+    l = { '<cmd>Telescope resume<cr>', 'Last Search' },
+    M = { '<cmd>Telescope man_pages<cr>', 'Man Pages' },
+    r = { '<cmd>Telescope oldfiles<cr>', 'Recent File' },
+    R = { '<cmd>Telescope registers<cr>', 'Registers' },
+    k = { '<cmd>Telescope keymaps<cr>', 'Keymaps' },
+    C = { '<cmd>Telescope commands<cr>', 'Commands' },
+  },
+
+  g = {
+    name = "Git",
+    g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
+    j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+    k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+    l = { "<cmd>GitBlameToggle<cr>", "Blame" },
+    p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+    r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+    R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+    s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+    u = {
+      "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+      "Undo Stage Hunk",
+    },
+    o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+    c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+    d = {
+      "<cmd>Gitsigns diffthis HEAD<cr>",
+      "Diff",
+    },
+    G = {
+      name = "Gist",
+      a = { "<cmd>Gist -b -a<cr>", "Create Anon" },
+      d = { "<cmd>Gist -d<cr>", "Delete" },
+      f = { "<cmd>Gist -f<cr>", "Fork" },
+      g = { "<cmd>Gist -b<cr>", "Create" },
+      l = { "<cmd>Gist -l<cr>", "List" },
+      p = { "<cmd>Gist -b -p<cr>", "Create Private" },
+    },
+  },
+
+  l = {
+    name = "LSP",
+    a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+    c = { "<cmd>lua require('user.lsp').server_capabilities()<cr>", "Get Capabilities" },
+    d = { "<cmd>TroubleToggle<cr>", "Diagnostics" },
+    w = {
+      "<cmd>Telescope lsp_workspace_diagnostics<cr>",
+      "Workspace Diagnostics",
+    },
+    f = { "<cmd>lua vim.lsp.buf.format({ async = true })<cr>", "Format" },
+    F = { "<cmd>LspToggleAutoFormat<cr>", "Toggle Autoformat" },
+    i = { "<cmd>LspInfo<cr>", "Info" },
+    h = { "<cmd>lua require('lsp-inlayhints').toggle()<cr>", "Toggle Hints" },
+    H = { "<cmd>IlluminationToggle<cr>", "Toggle Doc HL" },
+    I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
+    j = {
+      "<cmd>lua vim.diagnostic.goto_next({buffer=0})<CR>",
+      "Next Diagnostic",
+    },
+    k = {
+      "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>",
+      "Prev Diagnostic",
+    },
+    v = { "<cmd>lua require('lsp_lines').toggle()<cr>", "Virtual Text" },
+    l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+    o = { "<cmd>SymbolsOutline<cr>", "Outline" },
+    q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix" },
+    r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+    R = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
+    s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+    S = {
+      "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+      "Workspace Symbols",
+    },
+    t = { '<cmd>lua require("user.functions").toggle_diagnostics()<cr>', "Toggle Diagnostics" },
+    u = { "<cmd>LuaSnipUnlinkCurrent<cr>", "Unlink Snippet" },
   },
 }
 
