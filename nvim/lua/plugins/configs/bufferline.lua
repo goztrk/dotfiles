cf0c95ce9bf525f68d3a71d2a648d9a856a3e80a
goztrk <gokhan@eg-ozturk.com>
2022-09-22 00:50:03 +0300

    Stubbed theme toggler

--- nvim/lua/plugins/configs/bufferline.lua
+++ nvim/lua/plugins/configs/bufferline.lua
@@ -3,6 +3,21 @@ if not present then
   return
 end
 
+function Toggle_theme()
+  local style = require("tokyonight.config").options.style
+  if style == "day" then
+    require("tokyonight")._load("night")
+  else
+    require("tokyonight")._load("day")
+  end
+end
+
+vim.cmd [[
+  function Toggle_theme(a,b,c,d)
+    lua Toggle_theme()
+  endfunction
+]]
+
 vim.opt.termguicolors = true
 local options = {
   options = {
@@ -16,6 +31,14 @@ local options = {
         highlight = "Directory",
         separator = true,
       },
+    },
+    custom_areas = {
+      right = function()
+        return {
+          { text = "%@Toggle_theme@   %X" },
+          -- { text = "%@Quit_vim@ %X" },
+        }
+      end,
     },
   },
 }
