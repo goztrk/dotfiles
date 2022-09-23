137400c7356743792e48d62a848da50d3875d28c
goztrk <gokhan@eg-ozturk.com>
2022-09-23 21:21:22 +0300

    Add neovide settings

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -2,6 +2,6 @@
 local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
 vim.env.PATH = vim.env.PATH .. (is_windows and ";" or ":") .. vim.fn.stdpath "data" .. "/mason/bin"
 
+require "core.neovide"
 require "core.autocommands"
-
 require "core.mappings"
---
+++ nvim/lua/core/neovide.lua
@@ -0,0 +1,6 @@
+local g = vim.g
+if not g.neovide then
+  return
+end
+
+g.neovide_transparency = 0.9
